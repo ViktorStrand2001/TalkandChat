dc2f408f1563aa7a78aed2d59f5cfeaf40d02c87
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2024-11-23 00:54:00 +0100

    Add Maven wrapper and initial project setup

---
+++ src/main/java/com/talkandchat/TaC/TaCApplication.java
@@ -0,0 +1,13 @@
+package com.talkandchat.TaC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talkandchat/TaC/TaCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkandchat.TaC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
